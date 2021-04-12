ef7c97794f2dc0e7c80ea22763ce607f42416d42
Siete <sietefrouws@hotmail.com>
2021-04-12 17:34:28 +0200

    Improved naming convention a bit.
    Due to changed naming, files are not recognized as 'modified'.
    
    The naming format 'x_y_z-i_A_n_B_d_C' refers to:
    x,y,z: The start, stepsize and end of the varying variable, which can be n_elements (1_3_300) or displacement (-06_006_06, meaning from -0.06, by 0.006 till 0.06).
    A: The index within this set
    B: The n_elements value
    C: The displacement value (if not present, it's 0.02)

--- displaced_golden_angle_distribution.R
+++ displaced_golden_angle_distribution.R
@@ -11,7 +11,7 @@ plot_golden_angle_dist(
     apply_x_spread = F,
     apply_y_spread = F,
     save_file = F,
-    file_name_format = "fig/leaves_not_displaced/leave_1_3_300-%03.0f_n_%03.0f.png")
+    file_name_format = "fig/leaves_not_displaced/1_3_300-i_%03.0f_n_%03.0f.png")
 
 
 plot_golden_angle_dist(
@@ -20,27 +20,27 @@ plot_golden_angle_dist(
     apply_y_spread = F,
     draw_mode = 'bars',
     save_file = F,
-    file_name_format = "fig/vert_bars_not_displaced/1_3_300-%03.0f_n_%03.0f.png")
+    file_name_format = "fig/vert_bars_not_displaced/1_3_300-i_%03.0f_n_%03.0f.png")
 
 
 plot_golden_angle_dist(
     n_elements = c(1,2,3,4,5, 8, seq(10, 300, by = 3)),
     save_file = F,
-    file_name_format = "fig/leaves/leave_1_3_300-%03.0f_n_%03.0f.png")
+    file_name_format = "fig/leaves/1_3_300-i_%03.0f_n_%03.0f.png")
 
 
 plot_golden_angle_dist(
     n_elements = c(1,2,3,4,5, 8, seq(10, 300, by = 3)),
     draw_mode = 'bars',
     save_file = F,
-    file_name_format = "fig/vert_bars/1_3_300-%03.0f.png")
+    file_name_format = "fig/vert_bars/1_3_300-i_%03.0f_n_%03.0f.png")
 
 
 plot_golden_angle_dist(
     n_elements = 100,
     displacement = seq(-0.06, 0.06, length.out = 40),
     save_file = F,
-    file_name_format = "fig/leaves_displacement/leave_-06_006_06-i_%03.0f_n_%03.0f_d_%0.4f.png")
+    file_name_format = "fig/leaves_displacement/-06_006_06-i_%03.0f_n_%03.0f_d_%0.4f.png")
 
 
 plot_golden_angle_dist(
@@ -48,4 +48,4 @@ plot_golden_angle_dist(
     displacement = seq(-0.06, 0.06, length.out = 40),
     draw_mode = 'bars',
     save_file = F,
-    file_name_format = "fig/vert_bars_displacement/bar_-06_006_06-i_%03.0f_n_%03.0f_d_%0.4f.png")
+    file_name_format = "fig/vert_bars_displacement/-06_006_06-i_%03.0f_n_%03.0f_d_%0.4f.png")
